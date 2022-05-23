18b63bf06934b72b879e9cd73b957cd2976812b6
Martien Dermawan Tanama <martiendt@gmail.com>
2022-05-23 10:37:20 +0700

    add base middleware

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 
 This is a boilerplate application for building REST APIs in Node.js using Typescript, ESM and Express. Helps you stay productive by following best practices.
 
+## Features
+- **Compression** Using gzip compression with [Compression](https://github.com/expressjs/compression)
+- **CORS** Cross-Origin Resource-Sharing enabled using [Cors](https://github.com/expressjs/cors)
+- **Environment Variable** using [dotenv](https://www.npmjs.com/package/dotenv)
+- **Secure HTTP Headers** using [Helmet](https://github.com/helmetjs/helmet)
+
 ## Development Stack
 - [Node.js](https://nodejs.org) JavaScript run-time environment
 - [Express](https://expressjs.com) Node.js  framework
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,4 +1,7 @@
-import { Express } from "express";
+import compression from "compression";
+import cors from "cors";
+import { json, urlencoded, Express } from "express";
+import helmet from "helmet";
 import customMiddleware from "./custom-middleware/index.js";
 
 export default class Middleware {
@@ -7,7 +10,23 @@ export default class Middleware {
     this.app = app;
   }
 
+  private baseMiddleware() {
+    // Gzip compressing can greatly decrease the size of the response body
+    this.app.use(compression());
+    // Parse json request body
+    this.app.use(json());
+    // Parse urlencoded request body
+    this.app.use(urlencoded({ extended: false }));
+    // Set security HTTP headers
+    this.app.use(helmet());
+    // Cors
+    this.app.use(cors());
+  }
+
   register() {
+    this.baseMiddleware();
+
+    // Custom Middleware
     this.app.use(customMiddleware({ msg: "Helloworld" }));
   }
 }
