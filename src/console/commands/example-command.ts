4138f261505e36c778461d2c8f92e3dfafba1d7b
Martien Dermawan Tanama <martiendt@gmail.com>
2022-06-19 16:42:36 +0700

    update cli

--- src/console/commands/example-command.ts
+++ src/console/commands/example-command.ts
@@ -1,32 +1,27 @@
-import { BaseCommand, IArgument, IOption } from "@point-hub/express-cli";
+import { BaseCommand, IAttribute } from "@point-hub/express-cli";
 
 export class ExampleCommand extends BaseCommand {
-  cmdName(): string {
-    return "example";
+  attribute(): IAttribute {
+    return {
+      name: "example",
+      description: "description",
+      summary: "summary",
+      arguments: [
+        {
+          name: "<name>",
+          description: "Example Argument",
+        },
+      ],
+      options: [
+        {
+          flags: "-g, --greet <value>",
+          description: "Example Option",
+          default: "Hello",
+        },
+      ],
+    };
   }
-  cmdDescription(): string {
-    return "description";
-  }
-  cmdSummary(): string {
-    return "summary";
-  }
-  cmdArguments(): IArgument[] {
-    return [
-      {
-        name: "<name>",
-        description: "Example Arguments",
-      },
-    ];
-  }
-  cmdOptions(): IOption[] {
-    return [
-      {
-        flags: "-g, --greet <value>",
-        description: "Example Option",
-        default: "Hello",
-      },
-    ];
-  }
+
   async handle(name: string, options: object): Promise<void> {
     console.log("arguments", name);
     console.log("options", options);
